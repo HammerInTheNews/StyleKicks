e3a1379926c6c3db674e59b0e1e5e0ecc2fd6e9c
aljudson <alanjudson@gmail.com>
2013-09-05 15:58:21 -0400

    added shoe tests, part 2

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -3,4 +3,10 @@ class Shoe < ActiveRecord::Base
 
   validates :gender, presence: true
   validates :size, presence: true
+
+  def self.by_size(size)
+  	where("size LIKE ?", "#{{size}}%").order(:size)
+  end
+
+
 end
